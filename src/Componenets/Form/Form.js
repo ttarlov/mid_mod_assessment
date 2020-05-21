9efeb8e8748e463585eae9865b88f578d06af78a
Taras Tarlov <ttarlov@gmail.com>
2020-05-21 09:46:58 -0600

    Add Form Componanet file and initail structure

---
+++ src/Componenets/Form/Form.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Form.css'
+
+
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+
+
+    render() {
+
+        return(
+
+            <form>
+
+            </form>
+
+
+        )
+
+    }
+
+
+
+
+
+
+}
+
+
+export default Form
