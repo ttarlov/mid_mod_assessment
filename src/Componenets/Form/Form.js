f5b58ffeb35f0aeb11d03cb17d5fc6d122898b6f
Taras Tarlov <ttarlov@gmail.com>
2020-05-21 10:27:04 -0600

    Add onChange functionality to Form

--- src/Componenets/Form/Form.js
+++ src/Componenets/Form/Form.js
@@ -6,8 +6,14 @@ import './Form.css'
 class Form extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {name:'', date:'', time: '', number: ''}
     }
+
+
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+      };
 
 
 
@@ -16,7 +22,39 @@ class Form extends Component {
         return(
 
             <form>
-
+                <input
+                    type = 'text'
+                    name= 'name'
+                    value = {this.state.name}
+                    placeholder = 'Name'
+                    onChange = {this.handleChange}
+                    >
+                </input>
+                <input
+                    type = 'date'
+                    name= 'date'
+                    value = {this.state.date}
+                    placeholder = 'Date (mm/dd)'
+                    onChange = {this.handleChange}
+                    >
+                </input>
+                <input
+                    type = 'time'
+                    name= 'time'
+                    value = {this.state.time}
+                    placeholder = 'Time'
+                    onChange = {this.handleChange}
+                    >
+                </input>
+                <input
+                    type = 'number'
+                    name= 'number'
+                    value = {this.state.number}
+                    placeholder = 'Number of guests'
+                    onChange = {this.handleChange}
+                    >
+                </input>
+                <button>Make Reservation</button>
             </form>
 
 
