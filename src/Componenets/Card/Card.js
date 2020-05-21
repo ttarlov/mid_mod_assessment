434cab8317c2adedc1309d812e74f44e9cefd58a
Taras Tarlov <ttarlov@gmail.com>
2020-05-21 10:26:01 -0600

    Give Card Component Structure

---
+++ src/Componenets/Card/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Card.css'
+
+const Card = (props) => {
+    return (
+        <section className="res-card">
+            <h1>{props.name}</h1>
+            <p>{props.date}</p>
+            <p>{[props.time]}</p>
+            <p>{props.number}</p>
+        </section>
+
+
+    )
+}
+
+
+export default Card
